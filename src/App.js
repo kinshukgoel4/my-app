019ef7da4815bc5e771874e008214f1233229690
Kinshuk Goel <kinshukgoel4@gmail.com>
2022-12-18 00:15:08 +0530

    Update link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <a
           className="App-link"
           href="https://www.propelleraero.com/dirtmate/"
+          
           target="_blank"
           rel="noopener noreferrer"
         >
